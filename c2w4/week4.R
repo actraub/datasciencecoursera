88f583031896436de150470be9a08bab3c5a6b55
actraub <actraub@Kaseys-Air.fios-router.home>
2016-03-12 15:56:53 -0500

    part 1 of homework done

--- c2w4/week4.R
+++ c2w4/week4.R
@@ -1,10 +1,38 @@
+# Returns the best hospital in a state (e.g. NY, SC, etc) for "Heart Attack", "Heart Failure", "Pneumonia"
+# By Aaron Traub march 12 2016
+
+# best("TX", "heart failure")
+# best("MD", "Heart Attack"
+
 best <- function(state, outcome) {
-    
-    outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-        subset(outcome, outcome$State == state & head)
-    outcome$Hospital.Name
     ## Read outcome data
+    df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
     ## Check that state and outcome are valid
-    ## Return hospital name in that state with lowest 30-day death
+    if ((state %in% df$State ) & (outcome %in% c("Heart Attack", "heart failure", "Pneumonia"))) {
+        
+        re_from <- "\\b([[:lower:]])([[:lower:]]+)"
+        outcome <- gsub(re_from, "\\U\\1\\L\\2", outcome , perl=TRUE)
+        
+        # add a '.' if there is a ' '
+        outcome <- sub ("[ ]", ".", outcome) 
+        
+        #get column name to look at
+        outcome <- paste ("Hospital.30.Day.Death..Mortality..Rates.from.", outcome, sep = "")
+        
+        # get the proper columns
+        df.state <- subset(df, df$State == state)
+        
+        #return hospital with the mex value for the column
+        df.state.hospital <- df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
+        df.state.hospital
+    
+    } else {
+        
+        stop("Invalid type" )
+    }
+   
+        ## Return hospital name in that state with lowest 30-day death
+    
     ## rate
 }
