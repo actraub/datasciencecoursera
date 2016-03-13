58599d9148de02b7dde0e4ac040185969afc5d77
actraub <actraub@Kaseys-Air.fios-router.home>
2016-03-12 22:49:00 -0500

    second function almost done

--- c2w4/week4.R
+++ c2w4/week4.R
@@ -36,52 +36,68 @@ best <- function(state, outcome) {
 #The function reads the outcome-of-care-measures.csv file and returns a character vector with the name
 #of the hospital that has the ranking specified by the num argument. For example, the call
 
-#rankhospital("MD", "heart failure", 5)
+
+# > rankhospital("TX", "heart failure", 4)
+# [1] "DETAR HOSPITAL NAVARRO"
+
+# > rankhospital("MD", "heart attack", "worst")
+# [1] "HARFORD MEMORIAL HOSPITAL"
+
+# > rankhospital("MN", "heart attack", 5000)
+# [1] NA
+
 
 
 rankhospital <- function(state, outcome, num = "best") {
     ## Read outcome data
-    df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    df <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings="Not Available")
     ## Check that state and outcome are valid
-    if ((state %in% df$State ) & (outcome %in% c("Heart Attack", "heart failure", "Pneumonia"))) {
-        re_from <- "\\b([[:lower:]])([[:lower:]]+)"
-        outcome <- gsub(re_from, "\\U\\1\\L\\2", outcome , perl=TRUE)
+
+    re_from <- "\\b([[:lower:]])([[:lower:]]+)"
+    outcome <- gsub(re_from, "\\U\\1\\L\\2", outcome , perl=TRUE)
+    
+    # add a '.' if there is a ' '
+    outcome <- sub ("[ ]", ".", outcome) 
+    
+    #get column name to look at
+    outcome <- paste ("Hospital.30.Day.Death..Mortality..Rates.from.", outcome, sep = "")
+    
+    # get the proper ROWS
+    df.state <- subset(df, df$State == state)
+    df.state <- na.omit(df.state)
+    df.state <- df.state[ order( df.state[[outcome]], df.state$Hospital.Name, decreasing=TRUE), ]
+    
+    
+    if (num == "best") {
+        print("best")
+        # df.state.hospital <- df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
+        # df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
+        num <- 1
+    } else if (num == "worst") {
+        print("worst")
+        # df.state.hospital <- df.state[which.max(df.state[[outcome]]), "Hospital.Name"]
+        # df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
+        num <- nrow(df.state)
+    } else if ( num <= nrow(df.state)) {
+        print(num)
+        # num <- nrow(df.state) - num
+        # df.state <- df.state[order(df.state[[outcome]]),]
         
-        # add a '.' if there is a ' '
-        outcome <- sub ("[ ]", ".", outcome) 
-        
-        #get column name to look at
-        outcome <- paste ("Hospital.30.Day.Death..Mortality..Rates.from.", outcome, sep = "")
-        
-        # get the proper ROWS
-        df.state <- subset(df, df$State == state)
-        df <- df[order(df.state[[outcome]]),]
-        df <- df[order(df.state[[outcome]]),]
-        if (num == "best") {
-            print("best")
-            df.state.hospital <- df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
-            df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
-            
-        } else if (num == "worst") {
-            print("worst")
-            df.state.hospital <- df.state[which.max(df.state[[outcome]]), "Hospital.Name"]
-            df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
-        } else if ( num <= nrow(df.state)) {
-            print(num)
-            df <- df[order(df.state[[outcome]]),]
-        } else {
-            print("1")
-            stop("problemo****")
-        }
-        
-        
-        
+    } else {
+       
+        print("1")
+        stop("problemo****")
+    }
+    
+    df.state[num, "Hospital.Name"]
+    
+    
         
        # df.state.hospital <- df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
-    }
+}
     ## Return hospital name in that state with the given rank
     ## 30-day death rate
-}
+
 
 
 
