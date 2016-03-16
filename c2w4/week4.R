30ef374f91f16a05f92ad7776107776f7024da26
actraub <actraub@Kaseys-Air.fios-router.home>
2016-03-15 20:45:05 -0400

    looks good
    but the third function still doesn’t work on my laptop

--- c2w4/week4.R
+++ c2w4/week4.R
@@ -53,6 +53,7 @@ rankhospital <- function(state, outcome, num = "best") {
     df <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings="Not Available")
     ## Check that state and outcome are valid
 
+    #Capitalizes first letter of each word
     re_from <- "\\b([[:lower:]])([[:lower:]]+)"
     outcome <- gsub(re_from, "\\U\\1\\L\\2", outcome , perl=TRUE)
     
@@ -65,40 +66,66 @@ rankhospital <- function(state, outcome, num = "best") {
     # get the proper ROWS
     df.state <- subset(df, df$State == state)
     df.state <- na.omit(df.state)
-    df.state <- df.state[ order( df.state[[outcome]], df.state$Hospital.Name, decreasing=TRUE), ]
-    
-    
+    df.state <- df.state[ order( as.numeric(df.state[[outcome]]), df.state[["Hospital.Name"]], decreasing=FALSE), ]
     if (num == "best") {
         print("best")
-        # df.state.hospital <- df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
-        # df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
         num <- 1
     } else if (num == "worst") {
         print("worst")
-        # df.state.hospital <- df.state[which.max(df.state[[outcome]]), "Hospital.Name"]
-        # df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
         num <- nrow(df.state)
     } else if ( num <= nrow(df.state)) {
         print(num)
-        # num <- nrow(df.state) - num
-        # df.state <- df.state[order(df.state[[outcome]]),]
-        
     } else {
-       
         print("1")
         stop("problemo****")
     }
-    
     df.state[num, "Hospital.Name"]
-    
-    
-        
-       # df.state.hospital <- df.state[which.min(df.state[[outcome]]), "Hospital.Name"]
 }
-    ## Return hospital name in that state with the given rank
-    ## 30-day death rate
+  
 
 
 
+
+# The function reads the outcome-of-care-measures.csv file and returns a 2-column data frame
+# containing the hospital in each state that has the ranking specified in num.
+
+# head(rankall("heart attack", 20), 10)
+# tail(rankall("pneumonia", "worst"), 3)
+# tail(rankall("heart failure"), 10)
+
+rankall <- function(outcome, num = "best") {
+    df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    ## Check that state and outcome are valid
+    
+    #Capitalizes first letter of each word
+    re_from <- "\\b([[:lower:]])([[:lower:]]+)"
+    outcome <- gsub(re_from, "\\U\\1\\L\\2", outcome , perl=TRUE)
+    # add a '.' if there is a ' '
+    outcome <- sub ("[ ]", ".", outcome) 
+    #get column name to look at
+    outcome <- paste ("Hospital.30.Day.Death..Mortality..Rates.from.", outcome, sep = "")
+    resultsDF <- data.frame(hospital=character(), state=character())
+    states <- unique(df$State)
+    for (state in states) {
+        df.state <- subset(df, df$State == state)
+        df.state <- df.state[ order( as.numeric(df.state[[outcome]]), df.state[["Hospital.Name"]], decreasing=FALSE), ]
+        temp.vector <- data.frame()
+        if (num == "best") {
+            num <- 1
+        } else if (num == "worst") {
+            num <- nrow(df.state)
+        }
+        temp.vector <- c(df.state[num, "Hospital.Name"], state)
+        as.vector(temp.vector)
+        print(dim(temp.vector))
+        print (temp.vector)
+        resultsDF <- rbind(resultsDF, temp.vector )
+        # print (resultsDF)
+    }
+    colnames(resultsDF) <- c("Hospital", "state")
+    resultsDF
+}
+
+#Helper function to return the rank of a single state
 
     
